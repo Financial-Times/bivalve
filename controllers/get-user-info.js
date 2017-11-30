45dbf56736a1aaae83b95b94e9666311b64db2e3
Matt Brennan <matt@153.io>
2017-11-30 16:00:27 +0000

    dummy data

--- controllers/get-user-info.js
+++ controllers/get-user-info.js
@@ -1,1 +1,116 @@
-module.exports = () => ({hello: 'there'});
+module.exports = () => ({
+	"id": 1,
+	"pseudonym": "Anonymous User",
+	"timezone": "Europe\/London",
+	"avatar": "https:\/\/secure.gravatar.com\/avatar\/beb21939f6a5a1e3b48faa2d5eed358a?d=mm",
+	"role": "anonymous",
+	"searchviews": [{
+		"id": 1,
+		"offsetlimit": "0,50",
+		"querydefid": 1,
+		"dateread": {
+			"date": "2014-01-06 18:05:32.000000",
+			"timezone_type": 3,
+			"timezone": "Europe\/London"
+		},
+		"islocked": false,
+		"isprimary": false,
+		"perpage": 50,
+		"query": "",
+		"dispopts": "",
+		"sort": "date",
+		"title": "All",
+		"unreadcount": 0,
+		"classname": null
+	}, {
+		"id": 2,
+		"offsetlimit": "0,50",
+		"querydefid": 2,
+		"dateread": {
+			"date": "2014-01-06 18:05:32.000000",
+			"timezone_type": 3,
+			"timezone": "Europe\/London"
+		},
+		"islocked": false,
+		"isprimary": false,
+		"perpage": 50,
+		"query": "tag:us",
+		"dispopts": "",
+		"sort": "date",
+		"title": "US",
+		"unreadcount": 0,
+		"classname": null
+	}, {
+		"id": 3,
+		"offsetlimit": "0,50",
+		"querydefid": 3,
+		"dateread": {
+			"date": "2014-01-06 18:05:32.000000",
+			"timezone_type": 3,
+			"timezone": "Europe\/London"
+		},
+		"islocked": false,
+		"isprimary": false,
+		"perpage": 50,
+		"query": "tag:europe",
+		"dispopts": "",
+		"sort": "date",
+		"title": "Eurozone",
+		"unreadcount": 0,
+		"classname": null
+	}, {
+		"id": 4,
+		"offsetlimit": "0,50",
+		"querydefid": 4,
+		"dateread": {
+			"date": "2014-01-06 18:05:32.000000",
+			"timezone_type": 3,
+			"timezone": "Europe\/London"
+		},
+		"islocked": false,
+		"isprimary": false,
+		"perpage": 50,
+		"query": "tag:asia",
+		"dispopts": "",
+		"sort": "date",
+		"title": "Asia",
+		"unreadcount": 0,
+		"classname": null
+	}, {
+		"id": 5,
+		"offsetlimit": "0,50",
+		"querydefid": 5,
+		"dateread": {
+			"date": "2014-01-06 18:05:32.000000",
+			"timezone_type": 3,
+			"timezone": "Europe\/London"
+		},
+		"islocked": false,
+		"isprimary": false,
+		"perpage": 50,
+		"query": "tag:economy",
+		"dispopts": "",
+		"sort": "date",
+		"title": "Economy",
+		"unreadcount": 0,
+		"classname": null
+	}, {
+		"id": 6,
+		"offsetlimit": "0,50",
+		"querydefid": 6,
+		"dateread": {
+			"date": "2014-01-06 18:05:32.000000",
+			"timezone_type": 3,
+			"timezone": "Europe\/London"
+		},
+		"islocked": false,
+		"isprimary": false,
+		"perpage": 50,
+		"query": "tag:companies",
+		"dispopts": "",
+		"sort": "date",
+		"title": "Companies",
+		"unreadcount": 0,
+		"classname": null
+	}]
+});
