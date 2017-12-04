03c8d0b4f362bd007f54d912d495cb840bf7d9d7
Matt Brennan <matt@153.io>
2017-12-04 10:42:48 +0000

    move into file

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,4 @@
 const {search} = require('@financial-times/n-es-client');
-const pick = require('lodash.pick');
 
 const FASTFT_STREAM_ID = '5c7592a8-1f0c-11e4-b0cb-b2227cce2b54';
 
@@ -31,89 +30,6 @@ const parseQuery = types => (query = '') => {
 	}
 
 	return types.default;
-}
-
-class ResultMapper {
-	constructor(data, options = {}) {
-		this._data = data;
-		this._fields = options.outputfields || this._defaultFields();
-	}
-
-	_defaultFields() {
-		return Object.getOwnPropertyNames(
-			this.constructor.prototype
-		).filter(name =>
-			!name.startsWith('_') && name !== 'constructor'
-		);
-	}
-
-	toJSON() {
-		return pick(this, this._fields);
-	}
-}
-
-class Item extends ResultMapper {
-	get id() {
-		return this._data.id;
-	}
-
-	get abstract() {
-		return this._data.openingHTML;
-	}
-
-	get content() {
-		return this._data.bodyHTML;
-	}
-
-	get attachments() {
-		return [];
-	}
-
-	get currentversion() {
-		return 1;
-	}
-
-	get issticky() {
-		return false;
-	}
-
-	get datepublished() {
-		return new Date(this._data.publishedDate).getTime() / 1000;
-	}
-
-	get metadata() {
-		return {
-			primarytagid: this._data.displayConcept.id === FASTFT_STREAM_ID ? void(0) : this._data.displayConcept.id,
-		};
-	}
-
-	get shorturl() {
-		return this._data.webUrl;
-	}
-
-	get sortval() {
-		return this.datepublished.toString();
-	}
-
-	get status() {
-		return 'live';
-	}
-
-	get tags() {
-		return conceptsToTags(this._data.annotations);
-	}
-
-	get title() {
-		return this._data.title;
-	}
-
-	get url() {
-		return this._data.webUrl;
-	}
-
-	get uuidv3() {
-		return this._data.id;
-	}
 }
 
 const queryToES = parseQuery({
