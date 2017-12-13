cc5e0b95a207ac25ec590fba189dea131dcd143f
Matt Brennan <matt@153.io>
2017-12-13 15:47:11 +0000

    'fix' tests

--- src/controllers/test-error.js
+++ src/controllers/test-error.js
@@ -2,6 +2,6 @@
 
 import type {TestError, TestErrorResult, Controller} from './types';
 
-const testErrorController: Controller<TestError, TestErrorResult> = async () => { throw new Error('error test') };
+const testErrorController: Controller<TestError, TestErrorResult> = () => { throw new Error('error test') };
 
 module.exports = testErrorController;
