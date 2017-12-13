38bddf2921e61bd56d6499e825afd2581d2c8bcc
Matt Brennan <matt@153.io>
2017-12-13 15:56:00 +0000

    rudimentary caching

--- src/controllers/add-query-subscription.js
+++ src/controllers/add-query-subscription.js
@@ -2,10 +2,17 @@
 
 import type {AddQuerySubscription, AddQuerySubscriptionResult, Controller, RequestMeta} from './types';
 
-const addQuerySubscriptionController: Controller<AddQuerySubscription, AddQuerySubscriptionResult> = async (args, meta) => ({
-	queryid: 1,
-	channel: 'blogs.blog-335.all-posts',
-	appkey:'00960339489d6e143949'
-});
+const addQuerySubscriptionController: Controller<
+	AddQuerySubscription,
+	AddQuerySubscriptionResult
+> = async (args, {res}) => {
+	res.setHeader('cache-control', 'public, max-age=86400, must-revalidate, no-transform');
+
+	return {
+		queryid: 1,
+		channel: 'blogs.blog-335.all-posts',
+		appkey:'00960339489d6e143949',
+	};
+};
 
 module.exports = addQuerySubscriptionController;
--- src/controllers/get-user-info.js
+++ src/controllers/get-user-info.js
@@ -2,31 +2,17 @@
 
 import type {GetUserInfo, GetUserInfoResult, Controller} from './types';
 
-const getUserInfoController: Controller<GetUserInfo, GetUserInfoResult> = async () => ({
-	id: 1,
-	pseudonym: 'Anonymous User',
-	timezone: 'Europe/London',
-	avatar: 'https://secure.gravatar.com/avatar/beb21939f6a5a1e3b48faa2d5eed358a?d=mm',
-	role: 'anonymous',
-	searchviews: ['All', 'US', 'Eurozone', 'Asia', 'Economy', 'Companies'].map((term, i) => ({
-		id: i + 1,
-		offsetlimit: '0,50',
-		querydefid: i + 1,
-		dateread: {
-			date: '2014-01-06 18:05:32.000000',
-			timezone_type: 3,
-			timezone: 'Europe/London',
-		},
-		islocked: false,
-		isprimary: false,
-		perpage: 50,
-		query: term === 'All' ? '' : `tag:${term.toLowerCase()}`,
-		dispopts: '',
-		sort: 'date',
-		title: term,
-		unreadcount: 0,
-		classname: null
-	})),
-});
+const getUserInfoController: Controller<GetUserInfo, GetUserInfoResult> = async (args, {res}) => {
+	res.setHeader('cache-control', 'public, max-age=86400, must-revalidate, no-transform');
+
+	return {
+		id: 1,
+		pseudonym: 'Anonymous User',
+		timezone: 'Europe/London',
+		avatar: 'https://secure.gravatar.com/avatar/beb21939f6a5a1e3b48faa2d5eed358a?d=mm',
+		role: 'anonymous',
+		searchviews: [],
+	};
+};
 
 module.exports = getUserInfoController;
