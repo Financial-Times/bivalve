779f307d82548cd8dd91a8ccc9831ed0e404972e
Matt Brennan <matt@153.io>
2018-01-05 12:21:28 +0000

    rudimentary types for n-es-client

---
+++ flow-typed/n-es-client.js
@@ -0,0 +1,23 @@
+// @flow
+
+import type {FtItem} from '@financial-times/n-flow-ft-content';
+
+declare module '@financial-times/n-es-client' {
+	declare export type ESQuery = {
+		term: {
+			[string]: string
+		}
+	} | {
+		bool: {
+			must: Array<ESQuery>
+		}
+	};
+
+	declare type ESSearch = {
+		query: ESQuery,
+		size: number,
+		from: number,
+	};
+
+	declare export function search(ESSearch): Promise<Array<FtItem>>
+}
